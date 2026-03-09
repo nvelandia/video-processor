0fa4c33a880f4bdc6ada2fcb79d921efd497812f
nvelandia <nvelandia97@gmail.com>
2026-03-09 17:31:06 -0300

    Initial commit

---
+++ test/video-processor.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VideoProcessor from '../lib/video-processor-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/video-processor-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VideoProcessor.VideoProcessorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
